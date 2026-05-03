4686609648735249dcac6f227b5a8ceee01175b8
rifardin <fkfardin900@gmail.com>
2026-05-03 17:48:45 +0600

    test 2

--- proxy.js
+++ proxy.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import { headers } from "next/headers";
 import { auth } from "@/lib/auth";
 
-export async function middleware(request) {
+export async function proxy(request) {
     const session = await auth.api.getSession({
         headers: await headers(),
     });
